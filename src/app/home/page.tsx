6fb8e07407892992f8780d78cc04b1c9c5acc0b0
Tuấn Anh <anhpht@iceteasoftware.com>
2024-12-30 15:33:24 +0700

    add image

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,14 @@
+import Image from "next/image";
 import React from "react";
 
 export default function index() {
-  return <div>Home </div>;
+  return (
+    <div>
+      <Image
+        src="https://d1hjkbq40fs2x4.cloudfront.net/2017-08-21/files/landscape-photography_1645-t.jpg"
+        alt="My image"
+        width={300}
+      />
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import Image from "next/image";
 import React from "react";
 
 export default function page() {
-  return <div>Trang chủ</div>;
+  return (
+    <div>
+      <h1>trang chu</h1>
+      <Image
+        src="https://d1hjkbq40fs2x4.cloudfront.net/2017-08-21/files/landscape-photography_1645-t.jpg"
+        alt="My image"
+        width={300}
+      />
+    </div>
+  );
 }
